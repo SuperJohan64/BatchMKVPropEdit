c76873df1f7ec8c69666d9b144e37997d5356933
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2020-08-01 18:19:27 -0400

    Update BatchMKVPropEdit.ps1

--- BatchMKVPropEdit.ps1
+++ BatchMKVPropEdit.ps1
@@ -6,7 +6,7 @@ $sourceDir = $sourceDir.Replace("`"","")
 $track = Read-Host "`n********************************`n`nSelect track.`n`nFor subtitle tracks use s1, s2, s3 etc.`nFor audio tracks use a1, a2, a3, etc`n`nEnter your track"
 $language = Read-Host "`n********************************`n`nEnter the track 3 digit language code (eg eng or jpn)"
 $enableOrDisable = Read-Host "`n********************************`n`nSelect an option.`n`n0. Disable Track`n1. Enable Track`n`nEnter 0 or 1"
-$force = Read-Host "`n********************************`n`nSet this track as the default?`n`n0. No`n1. Yes`n`nEnter 0 or 1"
+$force = Read-Host "`n********************************`n`nForce this track?`n`n0. No`n1. Yes`n`nEnter 0 or 1"
 
 foreach ($file in (Get-ChildItem -LiteralPath $sourceDir -Recurse).FullName) {
     Write-Host `n$file
