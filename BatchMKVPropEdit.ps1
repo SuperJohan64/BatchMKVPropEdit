3351e9bbd1fe02ad6b4b0557aa770412dd0998bf
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2023-10-14 15:48:06 -0400

    Update BatchMKVPropEdit.ps1

--- BatchMKVPropEdit.ps1
+++ BatchMKVPropEdit.ps1
@@ -1,4 +1,16 @@
-$mkvpropedit = "$PSScriptRoot\mkvpropedit.exe"
+if (Test-Path "$PSScriptRoot\mkvpropedit.exe") {
+    $mkvpropedit = "$PSScriptRoot\mkvpropedit.exe"
+}
+else {
+    if (Test-Path "$env:ProgramFiles\MKVToolNix\mkvpropedit.exe") {
+        $mkvpropedit = "$env:ProgramFiles\MKVToolNix\mkvpropedit.exe"
+    }
+    else {
+        Write-Host "No mkvpropedit found. Please make sure mkvpropedit.exe is installed or saved to the same directory as this script."
+        Exit
+    }
+}
+Write-Host "mkvpropedit.exe found in $mkvpropedit"
 
 $sourceDir = Read-Host "`nEnter the path to the source file or directory"
 $sourceDir = $sourceDir.Replace("`"","")
