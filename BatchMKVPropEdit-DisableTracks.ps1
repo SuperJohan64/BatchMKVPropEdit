34ccf6e7428d3346a70705f4ab9ea7ef74e343e6
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2023-10-22 11:13:31 -0400

    10/22/2023

--- BatchMKVPropEdit-DisableTracks.ps1
+++ BatchMKVPropEdit-DisableTracks.ps1
@@ -16,17 +16,11 @@ $sourceDir = Read-Host "`nEnter the path to the source file or directory"
 $sourceDir = $sourceDir.Replace("`"","")
 
 $track = Read-Host "`n********************************`n`nSelect track.`n`nFor subtitle tracks use s1, s2, s3 etc.`nFor audio tracks use a1, a2, a3, etc`n`nEnter your track"
-# $language = Read-Host "`n********************************`n`nEnter the track 3 digit language code (eg eng or jpn)"
 $enableOrDisable = Read-Host "`n********************************`n`nSelect an option.`n`n0. Disable Track`n1. Enable Track`n`nEnter 0 or 1"
-$setDefaultTrack = Read-Host "`n********************************`n`nSelect an option.`n`n0. Remove default flag on track`n1. Enable default flag on track`n`nEnter 0 or 1"
-$forceTrack = Read-Host "`n********************************`n`nForce this track?`n`n0. No`n1. Yes`n`nEnter 0 or 1"
 
 foreach ($file in (Get-ChildItem -LiteralPath $sourceDir -Include *.mkv -Recurse).FullName) {
     Write-Host `n$file
     & $mkvpropedit $file `
         -d title `
         --edit track:$track --set flag-enabled=$enableOrDisable `
-        --edit track:$track --set flag-default=$setDefaultTrack `
-        --edit track:$track --set flag-forced=$forceTrack
-        # --edit track:$track --set language=$language
 }
